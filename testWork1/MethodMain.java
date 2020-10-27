e01a9fd0b8480f0da1717e957b00fb207a9344c6
Владислав <RubashkinVlad@yandex.ru>
2020-10-27 20:40:42 +0300

    new

--- testWork1/MethodMain.java
+++ testWork1/MethodMain.java
@@ -8,15 +8,12 @@ public class MethodMain {
     public static void main(String[] args) {
         FitnessClub fitnessClub = new FitnessClub();
 
-
+        for (int i = 0; i < 3; i++) {
             fitnessClub.addFitness(SeasonTicket.addSeasonTicket(),ZoneType.GROUPClass);
             fitnessClub.addFitness(SeasonTicket.addSeasonTicket(),ZoneType.POOL);
             fitnessClub.addFitness(SeasonTicket.addSeasonTicket(), ZoneType.GYM);
+        }
 
-
-
-
-//        Logger.getClientInfo();
         Logger.getFitnessInfo(fitnessClub);
 
         fitnessClub.isClosed();
